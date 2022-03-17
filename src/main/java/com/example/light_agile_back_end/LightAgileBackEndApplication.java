0dc6a35be214e79c96c67c6e6c880140d3bd02a8
Hồ Lê Minh Thạch <thach.holeminh@gmail.com>
2022-03-17 11:04:24 +0700

    Initial commit

---
+++ src/main/java/com/example/light_agile_back_end/LightAgileBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.light_agile_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightAgileBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightAgileBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/light_agile_back_end/LightAgileBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.light_agile_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightAgileBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
